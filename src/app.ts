f115951915160e81c381bad4daaa9b23f8230222
michael-lugashi <michael.lugashi@gmail.com>
2025-03-24 18:19:51 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { verifyToken } from '#middlewares/authentication.js';
 import authRouter from '#routes/authRoutes.js';
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouter);
