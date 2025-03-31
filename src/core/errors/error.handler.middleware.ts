0b1c0fe9cb4fb036a3a6196d6757174e28741b71
michael-lugashi <michael.lugashi@gmail.com>
2025-03-31 16:36:42 +0300

    added handling for internal server error

--- src/core/errors/error.handler.middleware.ts
+++ src/core/errors/error.handler.middleware.ts
@@ -1,5 +1,5 @@
 import { logger } from '../logger/logger.config.js';
-import { BaseError, UncaughtError } from './custom.errors.js';
+import { BaseError, InternalServerError, UncaughtError } from './custom.errors.js';
 import { getOriginalErrorDetails, normalizeError } from './error.services.js';
 import { ErrorHandlerMiddleware, ErrorResponse } from './error.types.js';
 
@@ -20,6 +20,11 @@ const errorHandler: ErrorHandlerMiddleware = (err, req, res, _next): void => {
     logger.error(logTitle, {
       ...logData,
       originalError: originalErrorDetails
+    });
+  } else if (error instanceof InternalServerError) {
+    logger.error(logTitle, {
+      ...logData,
+      message: error.logMessage
     });
   } else if (error.toLog) {
     logger.error(logTitle, logData);
