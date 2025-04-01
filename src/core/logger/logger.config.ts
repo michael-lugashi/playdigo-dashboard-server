15ed42ad3528a214a0cafb41a5b461cd457c29ff
michael-lugashi <michael.lugashi@gmail.com>
2025-04-01 11:45:06 +0300

    added error log format

--- src/core/logger/logger.config.ts
+++ src/core/logger/logger.config.ts
@@ -1,5 +1,27 @@
 import winston from 'winston';
 
+// Custom log format to make logs more readable with shortened stack traces
+const customLogFormat = winston.format.printf((info) =>
+  Object.entries(info).reduce(
+    (acc, [key, val]) => {
+      if (['level', 'message', 'timestamp'].includes(key)) {
+        return acc;
+      }
+
+      if (key === 'stack' && typeof val === 'string') {
+        return acc + `  ${key}: ${val.split('\n').slice(0, 3).join('\n')}\n`;
+      }
+
+      if (key === 'originalError' && typeof val === 'object') {
+        return acc + `  originalError: ${JSON.stringify(val)}\n`;
+      }
+
+      return acc + `  ${key}: ${String(val)}\n`;
+    },
+    `${String(info.timestamp)} [${info.level}]: ${String(info.message)}\n`
+  )
+);
+
 // Create a simple console logger
 export const logger = winston.createLogger({
   format: winston.format.combine(
@@ -7,11 +29,7 @@ export const logger = winston.createLogger({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
     winston.format.colorize(),
-    winston.format.printf((info) => {
-      const { level, message, timestamp, ...rest } = info;
-      const extraInfo = Object.keys(rest).length ? JSON.stringify(rest) : '';
-      return `${String(timestamp)} [${String(level)}]: ${String(message)} ${extraInfo}`;
-    })
+    customLogFormat
   ),
   level: 'info',
   transports: [new winston.transports.Console()]
