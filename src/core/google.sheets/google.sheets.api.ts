cbc0cf7d4179fb40d20997c3224e81ff3471bf10
michael-lugashi <michael.lugashi@gmail.com>
2025-04-10 17:54:54 +0300

    changed google auth way

--- src/core/google.sheets/google.sheets.api.ts
+++ src/core/google.sheets/google.sheets.api.ts
@@ -6,7 +6,7 @@ import {
 } from '#core/errors/custom.errors.js';
 import { sheets_v4 } from 'googleapis';
 
-import { googleSheetReqWrapper } from './google.sheets.auth.js';
+import sheets from './google.sheets.auth.js';
 import { CellValue, isUserData, SheetData, transformUserData, User, UserFunctions } from './google.sheets.types.js';
 
 const DATA_SHEET_ID = '1O7FYtZqZD548Pku5t6MeUfRBD6EU66G-0-ykQh2ezEE';
@@ -14,14 +14,12 @@ const USER_SHEET_ID = '1uliMkvCNzlncqH-ahh5u9QK5ebPWtXml_5r4cYPpW0g';
 
 export const getSheetData = async (sheetName: string, range: string): Promise<SheetData> => {
   try {
-    const res = await googleSheetReqWrapper((sheets) =>
-      sheets.spreadsheets.get({
-        fields: 'sheets(data(rowData(values(formattedValue,effectiveValue,userEnteredFormat))))',
-        includeGridData: true,
-        ranges: [`${sheetName}!A1:${range}`],
-        spreadsheetId: DATA_SHEET_ID
-      })
-    );
+    const res = await sheets.spreadsheets.get({
+      fields: 'sheets(data(rowData(values(formattedValue,effectiveValue,userEnteredFormat))))',
+      includeGridData: true,
+      ranges: [`${sheetName}!A1:${range}`],
+      spreadsheetId: DATA_SHEET_ID
+    });
 
     const sheet = res.data.sheets?.[0]?.data?.[0]?.rowData?.map((row) => {
       if (!row.values) {
@@ -61,12 +59,10 @@ export const generateUserFunctions = (): UserFunctions => {
     getUsers: async () => {
       if (cachedUsers) return cachedUsers;
 
-      const res = await googleSheetReqWrapper((sheets) =>
-        sheets.spreadsheets.values.get({
-          range: 'userData!A1:I',
-          spreadsheetId: USER_SHEET_ID
-        })
-      );
+      const res = await sheets.spreadsheets.values.get({
+        range: 'userData!A1:I',
+        spreadsheetId: USER_SHEET_ID
+      });
 
       const users = res.data.values;
       if (!users || !Array.isArray(users) || users.length < 2) {
--- src/core/google.sheets/google.sheets.auth.ts
+++ src/core/google.sheets/google.sheets.auth.ts
@@ -1,102 +1,35 @@
-import { BaseError, GoogleSheetsUncaughtError, InternalServerError } from '#core/errors/custom.errors.js';
-import { authenticate } from '@google-cloud/local-auth';
-import fs from 'fs/promises';
-import { Auth, google, sheets_v4 } from 'googleapis';
+import fs from 'fs';
+import { google } from 'googleapis';
 import path from 'path';
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-
-const TOKEN_PATH = path.join(process.cwd(), 'src/core/google.sheets/google.sheets.token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'src/core/google.sheets/google.sheets.credentials.json');
-
-interface InstalledCredentials {
+interface GoogleServiceAccountCredentials {
+  auth_provider_x509_cert_url: string;
+  auth_uri: string;
+  client_email: string;
   client_id: string;
-  client_secret: string;
-} /**
- * Load or request or authorization to call APIs.
- */
-async function authorize(): Promise<Auth.OAuth2Client> {
-  try {
-    const client = await loadSavedCredentialsIfExist();
-    if (client) {
-      return client;
-    }
-
-    const newClient = await authenticate({
-      keyfilePath: CREDENTIALS_PATH,
-      scopes: SCOPES
-    });
-    await saveCredentials(newClient);
-    return newClient;
-  } catch (error) {
-    if (error instanceof BaseError) {
-      throw error;
-    }
-    throw new GoogleSheetsUncaughtError(error);
-  }
+  client_x509_cert_url: string;
+  private_key: string;
+  private_key_id: string;
+  project_id: string;
+  token_uri: string;
+  type: 'service_account';
+  universe_domain: string;
 }
 
-const deleteToken = async () => {
-  clearCredentialsCache();
-  await fs.unlink(TOKEN_PATH);
-};
+const createSheetsApi = () => {
+  const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+  const CREDENTIALS_PATH = path.join(process.cwd(), 'src/core/google.sheets/google.sheets.credentials.json');
+  const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, 'utf8')) as GoogleServiceAccountCredentials;
 
-const genLoadSavedCredentialsIfExist = () => {
-  let cachedCredentials: Auth.OAuth2Client | null = null;
-  return {
-    clearCredentialsCache: () => {
-      cachedCredentials = null;
-    },
-    loadSavedCredentialsIfExist: async (): Promise<Auth.OAuth2Client | null> => {
-      try {
-        if (cachedCredentials) return cachedCredentials;
-        const content = await fs.readFile(TOKEN_PATH, 'utf-8');
-        const credentials = JSON.parse(content) as Auth.JWTInput;
-        cachedCredentials = google.auth.fromJSON(credentials) as Auth.OAuth2Client;
-        return cachedCredentials;
-      } catch {
-        return null;
-      }
-    }
-  };
-};
-
-const { clearCredentialsCache, loadSavedCredentialsIfExist } = genLoadSavedCredentialsIfExist();
-
-export async function googleSheetReqWrapper<T>(fn: (sheets: sheets_v4.Sheets) => Promise<T>): Promise<T> {
-  let auth = await authorize();
-  let sheets = google.sheets({ auth, version: 'v4' });
-  try {
-    return await fn(sheets);
-  } catch (err: unknown) {
-    const error = err as { response?: { data?: { error?: string } } };
-    const code = error.response?.data?.error;
-    if (code === 'invalid_grant') {
-      await deleteToken();
-      auth = await authorize();
-      sheets = google.sheets({ auth, version: 'v4' });
-      return await fn(sheets);
-    }
-    throw err;
-  }
-}
-
-async function saveCredentials(client: Auth.OAuth2Client): Promise<void> {
-  const content = await fs.readFile(CREDENTIALS_PATH, 'utf-8');
-  const keys = JSON.parse(content) as { installed?: InstalledCredentials; web?: InstalledCredentials };
-  const key = keys.installed ?? keys.web;
-  if (!key) throw new InternalServerError('No valid google sheets credentials found');
-
-  const payload = JSON.stringify({
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-    type: 'authorized_user'
+  const auth = new google.auth.JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: SCOPES
   });
 
-  await fs.writeFile(TOKEN_PATH, payload);
-}
+  const sheets = google.sheets({ auth, version: 'v4' });
+  return sheets;
+};
+
+const sheets = createSheetsApi();
+export default sheets;
